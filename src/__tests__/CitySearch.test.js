5ec156fde6ba944173e9b9112dd4d7bff94aa5d5
nirlepshah <nirlepshah@gmail.com>
2022-02-11 19:04:28 -0500

    Update CitySearch.test.js

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -8,8 +8,9 @@ describe('<CitySearch /> component', () => {
     let locations, CitySearchWrapper;
     beforeAll(() => {
         locations = extractLocations(mockData);
-        CitySearchWrapper = shallow(<CitySearch />);
+        CitySearchWrapper = shallow(<CitySearch locations={locations} />);
     });
+
     test('render text input', () => {
         expect(CitySearchWrapper.find('.city')).toHaveLength(1);
     });
@@ -44,14 +45,14 @@ describe('<CitySearch /> component', () => {
     });
     test('suggestion list match the query when changed', () => {
         CitySearchWrapper.setState({ query: '', suggestions: [] });
-        CitySearchWrapper.find('.city').simulate('change', {
-            target: { value: 'Berlin' }
+        CitySearchWrapper.find(".city").simulate("change", {
+            target: { value: "Berlin" },
         });
-        const query = CitySearchWrapper.state('query');
+        const query = CitySearchWrapper.state("query");
         const filteredLocations = locations.filter((location) => {
-            return locations.toUpperCase().indexOf(query.toUpperCase()) > -1;
+            return location.toUpperCase().indexOf(query.toUpperCase()) > -1;
         });
-        expect(CitySearchWrapper.state('suggestions')).toEqual(filteredLocations);
-    });
+        expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
+    })
 
 });
