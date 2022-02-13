1c3bd5d3fb6e44c3d4b5a1aea007916bd2e939bd
nirlepshah <nirlepshah@gmail.com>
2022-02-13 17:25:49 -0500

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -25,6 +25,8 @@ class CitySearch extends Component {
         this.setState({
             query: suggestion
         });
+
+        this.props.updateEvents(suggestion);
     }
 
     render() {
@@ -40,8 +42,8 @@ class CitySearch extends Component {
                     {this.state.suggestions.map((suggestion) => (
                         <li key={suggestion} onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
                     ))}
-                    <li key='all'>
-
+                    <li onClick={() => this.handleItemClicked("all")}>
+                        <b>See all cities</b>
                     </li>
                 </ul>
             </div>
