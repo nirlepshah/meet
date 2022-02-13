178a30a87ba10d734b3d92c4293265f1a5ed5c92
nirlepshah <nirlepshah@gmail.com>
2022-02-12 22:32:39 -0500

    Create NumberOfEvents.js

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+    state = {
+        numberOfEvents: ''
+
+    }
+    handleInputChanged = (event) => {
+
+        const value = event.target.value;
+
+        if (value < 1 || value > 32) {
+            this.setState({
+                numberOfEvents: '',
+
+            })
+        } else {
+            this.setState({
+                numberOfEvents: value,
+
+            });
+        }
+
+    };
+    render() {
+        return (
+            <div className="NumberOfEvents">
+
+                <p><b>Number of Events:</b></p>
+                <input
+                    type="number"
+                    name="number"
+                    className="number-of-events"
+
+                    onChange={(e) => this.handleInputChanged(e)}
+                />
+
+            </div>
+        );
+    }
+
+}
+export default NumberOfEvents;
