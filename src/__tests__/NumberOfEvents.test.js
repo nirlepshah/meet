f69d4d719e61efaae0f26f1bd679b1db77e3d815
nirlepshah <nirlepshah@gmail.com>
2022-02-13 18:17:46 -0500

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -11,12 +11,5 @@ describe('<NumberOfEvents /> component', () => {
     test('render text input', () => {
         expect(NumberOfEventsWrapper.find('.NumberOfEvents')).toHaveLength(1);
     });
-    test('render text input correctly from textbox', () => {
 
-        const numberOfEvents = NumberOfEventsWrapper.prop("numberOfEvents");
-
-        expect(NumberOfEventsWrapper.find(".number-of-events").prop("value")).toBe(
-            numberOfEvents
-        );
-    });
 })
