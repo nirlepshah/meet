9f8ca6857c2aead16f8d974c3551fa0e8e3b8cf6
nirlepshah <nirlepshah@gmail.com>
2022-02-16 19:21:47 -0500

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,25 +3,17 @@ import { shallow } from "enzyme";
 import NumberOfEvents from "../NumberOfEvents";
 
 describe('<NumberOfEvents /> component', () => {
-
     let NumberOfEventsWrapper;
     beforeAll(() => {
-        NumberOfEventsWrapper = shallow(<NumberOfEvents />)
+        NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     });
 
     test('render text input', () => {
-        expect(NumberOfEventsWrapper.find('.NumberOfEvents')).toHaveLength(1);
-    });
-
-    test('render text box', () => {
         expect(NumberOfEventsWrapper.find('.number-of-events')).toHaveLength(1);
     });
 
-    test("render text input correctly from prop", () => {
-        const numberOfEvents = NumberOfEventsWrapper.prop("numberOfEvents");
-        expect(NumberOfEventsWrapper.find(".number-of-events").prop("value")).toBe(
-            numberOfEvents
-        );
+    test('render text input correctly', () => {
+        const numberOfEvents = NumberOfEventsWrapper.prop('numberOfEvents');
+        expect(NumberOfEventsWrapper.find('.number-of-events').prop('value')).toBe(numberOfEvents);
     });
-
-})
+});
