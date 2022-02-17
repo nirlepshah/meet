0a915e3ac0f8d5ed5e2315d481a2aa3681b05c2e
nirlepshah <nirlepshah@gmail.com>
2022-02-16 19:17:16 -0500

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,43 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+
+
 
 class NumberOfEvents extends Component {
-    state = {
-        numberOfEvents: ''
 
-    }
-    handleInputChanged = (event) => {
-
-        const value = event.target.value;
-
-        if (value < 1 || value > 32) {
-            this.setState({
-                numberOfEvents: '',
-
-            })
-        } else {
-            this.setState({
-                numberOfEvents: value,
-
-            });
-        }
-
-    };
     render() {
         return (
             <div className="NumberOfEvents">
 
-                <p><b>Number of Events:</b></p>
-                <input
-                    type="number"
-                    name="number"
+                <p>Number of events to show:</p>
+                <input type="number"
+                    value={this.props.numberOfEvents}
                     className="number-of-events"
-
-                    onChange={(e) => this.handleInputChanged(e)}
-                />
+                    onChange={(e) => this.props.updateNumberOfEvents(e)} />
 
             </div>
         );
     }
-
 }
+
 export default NumberOfEvents;
