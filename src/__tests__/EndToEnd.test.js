fee5ef0fbef7e470b4c08ab7f26e9edd8e89a881
nirlepshah <nirlepshah@gmail.com>
2022-02-19 18:02:42 -0500

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -8,7 +8,9 @@ describe('show/hide an event details', () => {
         jest.setTimeout(30000);
 
         browser = await puppeteer.launch({
-
+            // headless: false,
+            //slowMo: 250, // slow down by 250ms
+            //ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
         });
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
@@ -20,6 +22,16 @@ describe('show/hide an event details', () => {
     });
 
     test('An event element is collapsed by default', async () => {
+        const eventDetails = await page.$('.event .details-view');
+        expect(eventDetails).toBeNull();
+    });
+    test('User can expand an event to see its details', async () => {
+        await page.click('.event .show-details');
+        const eventDetails = await page.$('.event .details-view');
+        expect(eventDetails).toBeDefined();
+    });
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.event .hide-details');
         const eventDetails = await page.$('.event .details-view');
         expect(eventDetails).toBeNull();
     });
