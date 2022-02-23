4b36a8b25acd3d08fbc67c160ce649909654041f
nirlepshah <nirlepshah@gmail.com>
2022-02-22 22:32:55 -0500

    Add WarningAlert when app is offline

--- src/EventList.js
+++ src/EventList.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import Event from './Event';
+import { WarningAlert } from './Alert';
+
 
 class EventList extends Component {
 
     render() {
         const { events } = this.props;
         return (
-            <ul className="EventList">
-                {events.map(event =>
-                    <li key={event.id}>
-                        <Event event={event} />
-                    </li>
+            <>
+                {!navigator.onLine ? (
+                    <WarningAlert text="You are offline, the events list has been loaded from the Cache!" />
+                ) : (
+                    ''
                 )}
-            </ul>
+                <ul className="EventList">
+                    {events.map(event =>
+                        <li key={event.id}>
+                            <Event event={event} />
+                        </li>
+                    )}
+                </ul>
+            </>
+
         );
     }
 }
