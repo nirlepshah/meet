5b8c5b5aedd6c0d9345b535ebb12c6007d0eaf9a
nirlepshah <nirlepshah@gmail.com>
2022-02-26 13:49:32 -0500

    Remove extra spacing in EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -5,7 +5,6 @@ import Event from '../Event';
 import { mockData } from '../mock-data'
 
 describe('<EventList /> component', () => {
-
     test('render correct number of events', () => {
         const EventListWrapper = shallow(<EventList events={mockData} />);
         expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
