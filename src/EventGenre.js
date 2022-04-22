26dd433a8212268daca80c70856a6650cdfeb643
nirlepshah <nirlepshah@gmail.com>
2022-04-21 20:56:53 -0400

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -18,16 +18,17 @@ export const EventGenre = ({ events }) => {
 
     return (
         <ResponsiveContainer className="container" height={400} >
-            <PieChart width={400} height={400}>
+            <PieChart width={200} height={200}>
                 <Pie
                     data={data}
                     cx={200}
                     cy={200}
                     labelLine={false}
-                    outerRadius={100}
+                    outerRadius={80}
                     fill="#1D4355"
                     dataKey="value"
-                    label={({ name, percent }) => (name !== 0 && percent !== 0) ? `${name}  ${(percent * 100).toFixed(0)}%` : ``}
+                    label={({ name, percent }) => (name !== 0 && percent !== 0) ? `${name} ${(percent * 100).toFixed(0)}%` : ``}
+
                 >
                 </Pie>
             </PieChart>
